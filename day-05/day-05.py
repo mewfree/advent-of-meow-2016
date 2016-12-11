1728a4fc9a32388d7a86c13d542f7c46072ba4aa
Damien Gonot <damien.gonot@gmail.com>
2016-12-10 23:03:24 -0500

    day 5 part 2 solution in Python

--- day-05/day-05.py
+++ day-05/day-05.py
@@ -14,4 +14,23 @@ while len(password) < 8:
 
     index += 1
 
+print('part 1:')
 print(password)
+
+index2 = 0
+password2 = ['', '', '', '', '', '', '', '']
+
+while len(''.join(password2)) < 8:
+    str_bytes = bytes(door_id + str(index2), encoding='utf-8')
+    hash = hashlib.md5(str_bytes).hexdigest()
+
+    if hash[:5] == '00000':
+        position = hash[5]
+        if position.isdigit() and int(position) < 8:
+            if password2[int(position)] == '':
+                password2[int(position)] = hash[6]
+
+    index2 += 1
+
+print('part 2:')
+print(''.join(password2))
